14d3060a1ad719c6dfca8f916579b2d01f521af7
leehyeon04 <donghyeon7621@naver.com>
2026-05-26 13:53:54 +0900

    변경

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // 태현하이 하이gogo
+    // 나도 변경해봄
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
