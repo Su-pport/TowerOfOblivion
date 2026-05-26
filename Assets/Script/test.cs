ef0e504102ee0fe95d779c427357045002bb1ef7
Su-pport <fkrhqpfktm@naver.com>
2026-05-26 13:25:58 +0900

    test

---
+++ Assets/Script/test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // 태현하이
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
