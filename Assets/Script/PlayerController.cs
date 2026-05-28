ae827552b12d5308273f14cf1668923d7daf66c2
정지원 <fkrhqpfktm@naver.com>
2026-05-28 11:04:36 +0900

    좌우 이동 애니메이션, 공격시 움직임 멈춤, 벽 충돌 설정

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerState
+    {
+        Idle,
+        Move,
+        Attack
+    }
 
     // 필요한 컴포넌트
     private Rigidbody2D myrigid;
     public InputManager input;
     private Animator animator;
+    private PlayerState state;
 
     // 이동 관련 변수
     [SerializeField] private float moveSpeed = 5f;
@@ -23,6 +31,8 @@ public class PlayerController : MonoBehaviour
         input.OnMove += StartMove;
         input.OnMoveEnd += EndMove;
         input.OnAttack += StartAttack;
+
+        state = PlayerState.Idle;
     }
 
     // Update is calㅁled once per frame
@@ -38,13 +48,23 @@ public class PlayerController : MonoBehaviour
     //start, in, end로 나눠서 작성
     private void StartMove(Vector2 moveInput)
     {
-        
-        InMove(moveInput);
+        if (state != PlayerState.Attack) {
+            state = PlayerState.Move;
+            InMove(moveInput);
+        }
     }
 
     private void InMove(Vector2 moveInput)
     {
         animator.SetFloat("RunState", 0.5f);
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
         targetPosition = myrigid.position + (moveInput.normalized * moveSpeed) * Time.fixedDeltaTime;
     }
 
@@ -56,6 +76,21 @@ public class PlayerController : MonoBehaviour
 
     private void StartAttack()
     {
+        state = PlayerState.Attack;
+        StartCoroutine(AttckCoroutine());
+    }
+
+    IEnumerator AttckCoroutine()
+    {
+        // 공격 애니메이션 재생
         animator.SetTrigger("Attack");
+        // 애니메이션이 끝날 때까지 대기
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        EndAttack();
+    }
+
+    private void EndAttack()
+    {
+        state = PlayerState.Idle;
     }
 } 
